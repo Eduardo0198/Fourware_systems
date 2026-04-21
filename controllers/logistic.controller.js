88e56110563179fb315342e2881b7b7e825138d6
EduardoViveros <A01710605@tec.mx>
2026-04-20 22:50:30 -0600

    CU-17: concecta controller de metricas losgisticas - Trabajado con Laura Cintora

--- controllers/logistic.controller.js
+++ controllers/logistic.controller.js
@@ -1,4 +1,6 @@
 const reservaModel = require('../models/reserva.model');
+const campaniaModel = require('../models/campania.model');
+const cuentaModel = require('../models/cuenta.model');
 const { registrarEvento } = require('../utils/auditoria.helper');
 const logger = require('../utils/logger');
 
@@ -33,6 +35,62 @@ function construirResumenReservas(reservas) {
     totalImporte: 0,
     totalPeso: 0,
     totalVolumen: 0
+  });
+}
+
+function normalizarIdFiltro(valor) {
+  const id = Number(valor);
+  return Number.isInteger(id) && id > 0 ? id : null;
+}
+
+function obtenerFiltrosMetricas(query) {
+  const { fechaInicio, fechaFin } = obtenerRangoFechas(query);
+  const agruparPor = query.agrupar_por === 'cuenta' ? 'cuenta' : 'campania';
+
+  return {
+    fechaInicio,
+    fechaFin,
+    agruparPor,
+    idCampania: normalizarIdFiltro(query.id_campania),
+    idCuenta: normalizarIdFiltro(query.id_cuenta)
+  };
+}
+
+function construirResumenMetricas(metricas) {
+  return (metricas || []).reduce((acc, item) => {
+    acc.totalReservas += Number(item.total_reservas || 0);
+    acc.totalProductos += Number(item.total_productos || 0);
+    acc.totalPeso += Number(item.peso_total || 0);
+    acc.totalVolumen += Number(item.volumen_total || 0);
+    acc.totalImporte += Number(item.importe_productos || 0);
+    return acc;
+  }, {
+    totalReservas: 0,
+    totalProductos: 0,
+    totalPeso: 0,
+    totalVolumen: 0,
+    totalImporte: 0
+  });
+}
+
+function construirFiltrosVista(filtros) {
+  return {
+    fecha_inicio: filtros.fechaInicio,
+    fecha_fin: filtros.fechaFin,
+    agrupar_por: filtros.agruparPor,
+    id_campania: filtros.idCampania || '',
+    id_cuenta: filtros.idCuenta || ''
+  };
+}
+
+function cargarCatalogosMetricas(callback) {
+  campaniaModel.obtenerTodas((errCampanias, campanias) => {
+    if (errCampanias) return callback(errCampanias);
+
+    cuentaModel.obtenerTodas((errCuentas, cuentas) => {
+      if (errCuentas) return callback(errCuentas);
+      callback(null, { campanias: campanias || [], cuentas: cuentas || [] });
+    });
   });
 }
 
@@ -88,8 +146,51 @@ exports.reservasConfirmadas = (req, res) => {
 };
 
 exports.metricas = (req, res) => {
-  registrarEvento(req, 'Consulta de métricas logísticas');
-  res.render('logistica/metricas');
+  const filtros = obtenerFiltrosMetricas(req.query);
+
+  const renderMetricas = (pageMessage, metricas, catalogos) => res.render('logistica/metricas', {
+    pageMessage,
+    filtros: construirFiltrosVista(filtros),
+    metricas: metricas || [],
+    resumen: construirResumenMetricas(metricas),
+    campanias: catalogos.campanias,
+    cuentas: catalogos.cuentas
+  });
+
+  cargarCatalogosMetricas((catalogosErr, catalogos) => {
+    const catalogosVista = catalogos || { campanias: [], cuentas: [] };
+
+    if (catalogosErr) {
+      logger.error(catalogosErr);
+      return renderMetricas({
+        tipo: 'danger',
+        texto: 'No fue posible cargar los filtros de campaña y cuenta.'
+      }, [], catalogosVista);
+    }
+
+    if (!esFechaInputValida(filtros.fechaInicio) || !esFechaInputValida(filtros.fechaFin) || filtros.fechaInicio > filtros.fechaFin) {
+      return renderMetricas({
+        tipo: 'danger',
+        texto: 'Debes seleccionar un periodo válido para consultar métricas logísticas.'
+      }, [], catalogosVista);
+    }
+
+    reservaModel.obtenerMetricasLogisticasConsolidadas(filtros, (err, metricas) => {
+      if (err) {
+        logger.error(err);
+        return renderMetricas({
+          tipo: 'danger',
+          texto: 'No fue posible consultar las métricas logísticas consolidadas.'
+        }, [], catalogosVista);
+      }
+
+      registrarEvento(req, 'Consulta de métricas logísticas consolidadas');
+      renderMetricas(metricas.length === 0 ? {
+        tipo: 'warning',
+        texto: 'No existen reservas confirmadas con los filtros seleccionados.'
+      } : null, metricas, catalogosVista);
+    });
+  });
 };
 
 exports.reporteOperativo = (req, res) => {
