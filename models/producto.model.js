35dda5a7a44f35ae9f0b91c26614c2b8282a7174
EduardoViveros <A01710605@tec.mx>
2026-04-23 06:46:59 -0600

    CU22-Agrego consultas y rutas para elimianr prodcuto

--- models/producto.model.js
+++ models/producto.model.js
@@ -165,6 +165,57 @@ exports.actualizarPorSku = (sku, producto, callback) => {
     ], callback);
 };
 
+exports.contarHistorialPorSku = (sku, callback) => {
+    const query = `
+        SELECT
+            (
+                SELECT COUNT(*)
+                FROM "Reserva_Producto"
+                WHERE "SKU" = ?
+            ) AS total_reservas,
+            (
+                SELECT COUNT(*)
+                FROM "Calificacion"
+                WHERE "SKU" = ?
+            ) AS total_calificaciones
+    `;
+
+    db.query(query, [sku, sku], (err, rows) => {
+        if (err) {
+            return callback(err);
+        }
+
+        const resumen = rows && rows.length ? rows[0] : {};
+        const totalReservas = Number(resumen.total_reservas || 0);
+        const totalCalificaciones = Number(resumen.total_calificaciones || 0);
+
+        callback(null, {
+            totalReservas,
+            totalCalificaciones,
+            total: totalReservas + totalCalificaciones
+        });
+    });
+};
+
+exports.retirarDelCatalogo = (sku, callback) => {
+    const query = `
+        UPDATE "Producto"
+        SET activo = 0
+        WHERE "SKU" = ?
+    `;
+
+    db.query(query, [sku], callback);
+};
+
+exports.eliminarPorSku = (sku, callback) => {
+    const query = `
+        DELETE FROM "Producto"
+        WHERE "SKU" = ?
+    `;
+
+    db.query(query, [sku], callback);
+};
+
 exports.contarProductos = (callback) => {
     db.query(`SELECT COUNT(*) AS total FROM "Producto"`, callback);
 };
